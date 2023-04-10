9eeae46c0c5e434925ba40db5a71e412d68de910
rodrigozandonadidbsnoop <rodrigo.zandonadi@dbsnoop.com>
2023-04-10 16:57:30 -0300

    Refactory because new features caused mistakes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   css: [
     'vuetify/lib/styles/main.sass',
     '@mdi/font/css/materialdesignicons.min.css'
-],
+  ],
   build: {
     transpile: ['vuetify'],
   },
